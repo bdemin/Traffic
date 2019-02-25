1a66a7eabf3d98398be7e3ebc78e9340236462ca
bdemin <42113340+bdemin@users.noreply.github.com>
2019-02-25 15:29:07 +0200

    Update main
    Switch to mpl animation

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ Created on Mon Oct 15 18:26:43 2018
 @author: slavd
 """
 
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.patches import Rectangle
-import numpy as np
+#from win32api import GetSystemMetrics
 
 class Vehicle(object):
     def __init__(self, radius):
@@ -21,17 +22,24 @@ class Vehicle(object):
         self.speed = 0
         self.max_speed = 2*np.pi/4
         self.acc = 2*np.pi/16
+        self.decc = self.acc*4
 
-        
+    def FixPosition(width, length):
+        pass
+    
     def FindPosition(self, angle):
         return (self.radius*np.cos(angle), self.radius*np.sin(angle))
     
     def Draw(self, ax):
         self.artist = ax.add_patch(Rectangle(self.position, self.W, self.L, np.rad2deg(self.angle)))
+#        if self.angle > 2*np.pi():
+#            self.angle -= 2*np.pi()
+        print(self.angle)
         return self.artist
 
     def CollisionWarning(self, next_car_angle):
-        if self.angle < next_car_angle + np.deg2rad(10):
+        safe_distance = np.deg2rad(20)
+        if self.angle + safe_distance >= next_car_angle:
             return True
         else:
             return False
@@ -44,34 +52,22 @@ class Vehicle(object):
                 self.speed += self.acc*dt
         else:
             print('brake!')
-            self.speed -= self.acc*dt
+            if self.speed >= 0:
+                self.speed -= self.decc*dt
+#            self.speed = 1
 
             
         self.angle += self.speed*dt
         self.position = self.FindPosition(self.angle)
         
-    
-#    def SlowDown(self):
-#        self.angle -= self.acc
-#        self.position = self.FindPosition(self.angle)
 
-    
 
-    
-    def FixPosition(width, length):
-        pass
-    
-    
-#fig1 = plt.figure(figsize=(28, 18), facecolor='w', edgecolor='g')
-#ax1 = plt.axes(xlim=(-5, 5), ylim=(-5, 5))
-#plt.xlim(-5,5)
-#plt.ylim(-5,5)
-#plt.axis('equal')
-#plt.axis([-3, 3, -3, 3])
 
 fig1, ax1 = plt.subplots()
-
-#fig1.set_size_inches(w = 8, h = 8)
+fig1.set_figheight(7)
+fig1.set_figwidth(7)
+#screen_width = GetSystemMetrics(0)
+#screen_height = GetSystemMetrics(1)
 radius = 3
 
 
@@ -80,7 +76,6 @@ T = 40
 FPS = 48
 totalFrames = T*FPS
 dt = (1/FPS)
-
 def animate(frame):
     print('current time:', frame*dt, 'sec')
     artists = []
@@ -88,13 +83,18 @@ def animate(frame):
         vehicles.append(Vehicle(radius))
     
 #    draw all
-    last_vehicle = vehicles[0]
+#    last_vehicle = vehicles[0]
     for vehicle in vehicles:
+#        if (len(vehicles) > 1):    
+        for other_vehicle in vehicles:
+            other_angle = other_vehicle.angle
+            if other_vehicle == vehicle:
+                vehicle.Drive(dt, 20)
+            else:
+                vehicle.Drive(dt, other_angle)
+                    
+                        
         artists.append(vehicle.Draw(ax1))
-#        print(vehicle.position,'\n')
-#        print(vehicle.angle,'\n')
-        vehicle.Drive(dt, last_vehicle.angle)
-        last_vehicle = vehicle
     return artists
 
 def init():
